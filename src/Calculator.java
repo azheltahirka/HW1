c2e2fc3b79369a579418aa2950af7d20286fa217
Andrei Zheltahirka <np9h1k@gmail.com>
2022-01-30 15:02:05 +0300

    best practice

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,16 +1,14 @@
 public class Calculator {
 
     public static void main(String[] args) {
-        EnterNumber enterNumber = new EnterNumber();
-        EnterNumberOfOperation enterNumberOfOperation = new EnterNumberOfOperation();
-        SelectOperation selectOperation = new SelectOperation();
-        Calculate calculate = new Calculate();
         System.out.println("Enter a first number");
-        double first = enterNumber.enterNumber();
+        double first = EnterNumber.enterNumber();
         System.out.println("Enter a second number");
-        double second = enterNumber.enterNumber();
+        double second = EnterNumber.enterNumber();
         System.out.println("Enter a number of Operation( +(1). -(2), *(3), /(4) )");
-        int operationNumber = enterNumberOfOperation.enterNumberOfOperation();
-        calculate.resultOperation(first,second,selectOperation.getOperation(operationNumber));
+        int operationNumber = EnterNumberOfOperation.enterNumberOfOperation();
+        Calculate calculate = new Calculate();
+        SelectOperation selectOperation = new SelectOperation();
+        calculate.resultOperation(first, second, selectOperation.getOperation(operationNumber));
     }
 }
